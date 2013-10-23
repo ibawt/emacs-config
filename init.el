1fc56ca9fbf3bd921dba6bcdea28ad37a5e2bdbb
Ian Quick <ian.quick@gmail.com>
2013-10-23 09:49:05 -0400

    my emacs config

---
+++ init.el
@@ -0,0 +1,276 @@
+(setenv "PATH" (concat (getenv "PATH") ":/opt/boxen/homebrew/bin"))
+(setq exec-path (append exec-path '("/opt/boxen/homebrew/bin")))
+(require 'cl)				; common lisp goodies, loop
+(setq rbenv-installation-dir "/opt/boxen/rbenv")
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(add-to-list 'load-path "~/.emacs.d/")
+(setq-default tab-width 2)
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") `comment-or-uncomment-region)
+(global-set-key (kbd "C-+") `text-scale-increase)
+(global-set-key (kbd "C--") `text-scale-decrease)
+(show-paren-mode t)
+
+(setq package-archives
+          '(("gnu" . "http://elpa.gnu.org/packages/")
+            ("marmalade" . "http://marmalade-repo.org/packages/")
+            ("melpa" . "http://melpa.milkbox.net/packages/")))
+;; now either el-get is `require'd already, or have been `load'ed by the
+;; el-get installer.
+
+;; set local recipes
+(setq
+ el-get-sources
+ '((:name buffer-move			; have to add your own keys
+	  :after (lambda ()
+		   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
+		   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
+		   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
+		   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))
+
+   (:name smex				; a better (ido like) M-x
+	  :after (lambda ()
+		   (setq smex-save-file "~/.emacs.d/.smex-items")
+		   (global-set-key (kbd "M-x") 'smex)
+		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
+
+   (:name goto-last-change		; move pointer back to last change
+	  :after (lambda ()
+		   ;; when using AZERTY keyboard, consider C-x C-_
+		   (global-set-key (kbd "C-x C-/") 'goto-last-change)))))
+
+;; now set our own packages
+(setq
+ my:el-get-packages
+ '(el-get				; el-get is self-hosting
+   escreen            			; screen for emacs, C-\ C-h
+   php-mode-improved			; if you're into php...
+   switch-window			; takes over C-x o
+   auto-complete			; complete as you type with overlays
+   zencoding-mode			; http://www.emacswiki.org/emacs/ZenCoding
+   color-theme		                ; nice looking emacs
+   color-theme-tango))	                ; check out color-theme-solarized
+;;
+;; Some recipes require extra tools to be installed
+;;
+;; Note: el-get-install requires git, so we know we have at least that.
+;;
+(when (el-get-executable-find "svn")
+  (loop for p in '(psvn    		; M-x svn-status
+		   yasnippet		; powerful snippet mode
+		   )
+	do (add-to-list 'my:el-get-packages p)))
+
+(setq my:el-get-packages
+      (append
+       my:el-get-packages
+       (loop for src in el-get-sources collect (el-get-source-name src))))
+(setq tab-width 2)
+;; install new packages and init already installed packages
+(el-get 'sync my:el-get-packages)
+(defun coffee-custom ()
+	"coffee-mode-hook"
+	(make-local-variable 'tab-width)
+	(set 'tab-width 2)
+	(auto-complete-mode t)
+	(autopair-mode)
+	)
+
+(add-hook 'coffee-mode-hook 'coffee-custom)
+
+(defun ruby-custom ()
+	"ruby-mode-hook"
+	(auto-complete-mode t)
+	(autopair-mode)
+	)
+(add-hook `ruby-mode-hook `ruby-custom)
+
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+
+(setq indent-tabs-mode nil)
+(add-hook 'html-mode '(progn
+			(setq sgml-basic-offset 2)
+			(setq indent-tabs-mode nil)
+))
+
+(add-hook 'rhtml-mode '(progn
+			 (setq tab-width 2
+			       indent-tabs-mode nil
+			       )
+			 ))
+
+;; on to the visual settings
+(setq inhibit-splash-screen t)		; no splash screen, thanks
+(line-number-mode 1)			; have line numbers and
+(column-number-mode 1)			; column numbers in the mode line
+
+(tool-bar-mode -1)			; no tool bar with icons
+(scroll-bar-mode -1)			; no scroll bars
+(unless (string-match "apple-darwin" system-configuration)
+  ;; on mac, there's always a menu bar drown, don't have it empty
+  (menu-bar-mode -1))
+
+;; choose your own fonts, in a system dependant way
+(if (string-match "apple-darwin" system-configuration)
+    (set-face-font 'default "Monaco-13")
+  (set-face-font 'default "Monospace-10"))
+
+(global-hl-line-mode)			; highlight current line
+(global-linum-mode 1)			; add line numbers on the left
+
+;; avoid compiz manager rendering bugs
+(add-to-list 'default-frame-alist '(alpha . 100))
+
+;; copy/paste with C-c and C-v and C-x, check out C-RET too
+(cua-mode)
+
+;; under mac, have Command as Meta and keep Option for localized input
+(when (string-match "apple-darwin" system-configuration)
+  (setq mac-allow-anti-aliasing t)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'none))
+
+;; Use the clipboard, pretty please, so that copy/paste "works"
+(setq x-select-enable-clipboard t)
+
+;; Navigate windows with M-<arrows>
+(windmove-default-keybindings 'meta)
+(setq windmove-wrap-around t)
+
+; winner-mode provides C-<left> to get back to previous window layout
+(winner-mode 1)
+
+;; whenever an external process changes a file underneath emacs, and there
+;; was no unsaved changes in the corresponding buffer, just revert its
+;; content to reflect what's on-disk.
+(global-auto-revert-mode 1)
+
+;; M-x shell is a nice shell interface to use, let's make it colorful.  If
+;; you need a terminal emulator rather than just a shell, consider M-x term
+;; instead.
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; If you do use M-x term, you will notice there's line mode that acts like
+;; emacs buffers, and there's the default char mode that will send your
+;; input char-by-char, so that curses application see each of your key
+;; strokes.
+;;
+;; The default way to toggle between them is C-c C-j and C-c C-k, let's
+;; better use just one key to do the same.
+(require 'term)
+(define-key term-raw-map  (kbd "C-'") 'term-line-mode)
+(define-key term-mode-map (kbd "C-'") 'term-char-mode)
+
+;; Have C-y act as usual in term-mode, to avoid C-' C-y C-'
+;; Well the real default would be C-c C-j C-y C-c C-k.
+(define-key term-raw-map  (kbd "C-y") 'term-paste)
+
+;; use ido for minibuffer completion
+(require 'ido)
+(ido-mode t)
+(setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
+(setq ido-enable-flex-matching t)
+(setq ido-use-filename-at-point 'guess)
+(setq ido-show-dot-for-dired t)
+
+;; default key to switch buffer is C-x b, but that's not easy enough
+;;
+;; when you do that, to kill emacs either close its frame from the window
+;; manager or do M-x kill-emacs.  Don't need a nice shortcut for a once a
+;; week (or day) action.
+(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
+(global-set-key (kbd "C-x C-c") 'ido-switch-buffer)
+(global-set-key (kbd "C-x B") 'ibuffer)
+(global-set-key (kbd "C-x f") 'find-file-in-repository)
+;; C-x C-j opens dired with the cursor right on the file you're editing
+(require 'dired-x)
+
+;; full screen
+(defun fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen
+		       (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
+(global-set-key [f11] 'fullscreen)
+(global-set-key (kbd "C-x C-z") 'magit-status)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(erc-modules (quote (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands notifications readonly ring services stamp track))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; IMAP, gmail:
+(setq elmo-imap4-default-server "imap.gmail.com"
+      elmo-imap4-default-user "ian.quick@jadedpixel.com"
+      elmo-imap4-default-authenticate-type 'clear
+      elmo-imap4-default-port '993
+      elmo-imap4-default-stream-type 'ssl
+
+      ;;for non ascii-characters in folder-names
+      elmo-imap4-use-modified-utf7 t)
+
+;; SMTP
+(setq wl-smtp-connection-type 'starttls
+      wl-smtp-posting-port 587
+      wl-smtp-authenticate-type "plain"
+      wl-smtp-posting-user "ian.quick"
+      wl-smtp-posting-server "smtp.gmail.com"
+      wl-local-domain "jadedpixel.com"
+      wl-message-id-domain "smtp.gmail.com")
+
+(setq wl-from "Ian Quick <ian.quick@jadedpixel.com>"
+
+      ;;all system folders (draft, trash, spam, etc) are placed in the
+      ;;[Gmail]-folder, except inbox. "%" means it's an IMAP-folder
+      wl-default-folder "%inbox"
+      wl-draft-folder   "%[Gmail]/Drafts"
+      wl-trash-folder   "%[Gmail]/Trash"
+      wl-fcc            "%[Gmail]/Sent"
+
+      ;; mark sent messages as read (sent messages get sent back to you and
+      ;; placed in the folder specified by wl-fcc)
+      wl-fcc-force-as-read    t
+
+      ;;for when auto-compleating foldernames
+      wl-default-spec "%")
+(require 'go-autocomplete)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(require 'auto-complete-config)
+(ac-config-default)
+(global-auto-complete-mode t)
+
+(defun untabify-buffer ()
+	(interactive)
+	(untabify (point-min) (point-max)))
+(defun indent-buffer ()
+	(interactive)
+	(indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+	"Clean the whitespace of a buffer"
+	(interactive)
+	(indent-buffer)
+	(untabify-buffer)
+	(delete-trailing-whitespace))
+
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
